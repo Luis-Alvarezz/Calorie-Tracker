f84a56c9d46105a61987a4fadd72c47dab7af0c3
Luis-Alvarezz <luisangelug10@gmail.com>
2025-10-14 12:30:46 -0600

    [MODIFY]: Seccion para Mostrar Calorias Consumidas, Quemadas y diferencia

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from "./components/Form"
 import { useReducer, useEffect, useMemo } from "react";
 import ActivityList from "./components/ActivityList";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState); // * De momento NO ocupamos el STATE
@@ -40,12 +41,21 @@ function App() {
         </div>
       </section>
 
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker 
+            activities={state.activities}
+          />
+        </div>
+      </section>
+
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList 
           activities={state.activities}
           dispatch={dispatch}
         />
       </section>
+
     </>
   )
 }

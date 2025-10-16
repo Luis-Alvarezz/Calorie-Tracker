5c7215f971921223715a96125b4f5af00d2caea2
Luis-Alvarezz <luisangelug10@gmail.com>
2025-10-15 23:43:10 -0600

    [ADD]: Componente para mostrar el color y texto de cada apartado de manera dinamica

--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -2,10 +2,19 @@ type CalorieDisplayProps = {
   caloriesConsumedAndBurned: number,
   text: string
 }
+
+const calorieColor: {[textKey: string]: string} = {
+  'Consumidas': 'text-lime-500',
+  'Quemadas': 'text-orange-500',
+  'Diferencia': 'text-red-500',
+}
+
 export default function CalorieDisplay({ caloriesConsumedAndBurned, text }: CalorieDisplayProps) {
+  const colorClass = calorieColor[text] || 'text-gray-500'
+  
   return (
     <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-      <span className={`font-black text-6xl ${text === 'Consumidas' ? 'text-orange-500' : 'text-lime-500'}`}> {caloriesConsumedAndBurned} </span>
+      <span className={`font-black text-6xl ${colorClass}`}> {caloriesConsumedAndBurned} </span>
       {text}
     </p>
   )

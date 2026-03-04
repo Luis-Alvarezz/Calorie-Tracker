8330c34d66282839143de2d5f67d46baac7a139b
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-03 21:56:41 -0600

    [MODIFY]: Se quitaron PROPs de Componente Form para implementar Context

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,8 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form
-            dispatch={dispatch}
-            state={state}
+            // dispatch={dispatch}
+            // state={state}
           />
         </div>
       </section>
@@ -53,8 +53,8 @@ function App() {
 
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList 
-          activities={state.activities}
-          dispatch={dispatch}
+          // activities={state.activities}
+          // dispatch={dispatch}
         />
       </section>
 

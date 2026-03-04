85eea41072ce6b1a6841b44acbc3e218c4a512c4
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-03 20:54:17 -0600

    tco

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ActivityProvider } from './context/ActivityContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ActivityProvider>
+      <App />
+    </ActivityProvider>
   </StrictMode>,
 )
+
+{/* <ActivityProvider> // * De esa forma los datos que vayamos teniendo en el **ActivityContext y ActivityProvider** van a estar disponibles en toda la aplicación. */}
